cf766b0257c667e646368bdbe75a66f4e71c1570
gabrielkoyamao <gabriel.koyama.alves@hotmail.com>
2019-08-28 08:49:13 -0300

    2808

--- lib/_bd.sql
+++ lib/_bd.sql
@@ -5,14 +5,14 @@ USE AUTH;
 CREATE TABLE IF NOT EXISTS tb_user(
 	usu_cd INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	usu_nm VARCHAR(50), 
-	usu_cpf VARCHAR(50)
+	usu_psw VARCHAR(64),
+	usu_email VARCHAR(50), 
+	usu_sobrenome VARCHAR(50),
+	usu_data_nasc TIMESTAMP,
+	usu_cpf VARCHAR(50) NOT NULL
 );
 
 -- ------------------- INSERTS TESTE ---------------------------
--- INSERT INTO tb_user (usu_nm, usu_cpf) VALUES ('Teste1', '123');
--- INSERT INTO tb_user (usu_nm, usu_cpf) VALUES ('Teste2', '123');
--- INSERT INTO tb_user (usu_nm, usu_cpf) VALUES ('Teste3', '123');
--- INSERT INTO tb_user (usu_nm, usu_cpf) VALUES ('Teste4', '123');
--- INSERT INTO tb_user (usu_nm, usu_cpf) VALUES ('Teste5', '123');
+INSERT INTO tb_user (usu_nm, usu_psw, usu_email, usu_sobrenome, usu_data_nasc, usu_cpf) VALUES ('usu_nm', 'usu_psw', 'usu_email', 'usu_sobrenome', '2017-06-15 09:34:21', 'usu_cpf');
 
-SELECT * FROM tb_user;
+-- SELECT * FROM tb_user;
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,61 @@
 const express 	= require('express');
-const router  	= express.Router();
+const sha256 	= require('sha256');
 const sql 		= require('../lib/sql');
+const router  	= express.Router();
 
-router.get('/', async function(req, res, next) {
-	// sql.findAllUsers(response => {
-	// 	res.send(response);
-	// });
+router.get('/', function(req, res, next) {
+	sql.findAllUsers(response => {
+		res.send(response);
+	});
+});
 
-	// sql.insertOne({nome: 'gabriel', cpf: '123435'}, response => {
-	// 	res.send('ok');
-	// });
+router.post('/add', function(req, res, next) {
+	var data = {
+		name: 		req.body.nome,
+		psw: 		req.body.senha,
+		email:  	req.body.email,
+		sobrenome: 	req.body.sobrenome,
+		data_nasc: 	new Date().toLocaleString('pt-BR').slice(0,-3),
+		cpf: 		req.body.cpf
+	}
+	sql.insertOne(data, response => {
+		res.send(200);
+	});
+});
 
+router.post('/delete', function(req, res, next) {
+	if(req.body.id){
+		sql.delete(req.body.id, function(err, response){
+			if(err) console.error(err)
+			console.log(err)
+			console.log(response)
+			res.send(200);
+		});
+	}else{
+		res.send(500);
+	}
+});
+
+router.post('/edit', function(req, res, next) {
+	// !!!!!!!!!! FAZENDO !!!!!!!!
+	if(req.body){
+		var data = {
+			id:  		req.body.id,
+			name: 		req.body.nome,
+			psw: 		req.body.senha,
+			email:  	req.body.email,
+			sobrenome: 	req.body.sobrenome,
+			data_nasc: 	new Date().toLocaleString('pt-BR').slice(0,-3),
+			cpf: 		req.body.cpf
+		}
+
+		sql.update(data, function(err, response){
+			if(err) console.error(err)
+			res.send(200);
+		});
+	}else{
+		res.send(500);
+	}
 });
 
 module.exports = router;
