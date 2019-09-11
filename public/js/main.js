ed7e10576e599c9fc352d280eb17fe6bb7e14937
GomesDosSantos <33328754+GomesDosSantos@users.noreply.github.com>
2019-09-11 08:43:13 -0300

    Prototype Format para String

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+
+String.prototype.format = function() {
+    
+    /* "{0}".format("a") ---> "a" */
+
+    a = this;
+    for (k in arguments) {
+      a = a.replace("{" + k + "}", arguments[k])
+    }
+    return a
+}
+
+  
